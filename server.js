387db4778b12665780c37c7fe797a52278f8d2e8
frsargua <frsargua@gmail.com>
2022-07-30 00:07:44 +0100

    added express-session

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers/index");
 const sequelize = require("./config/connection");
 
@@ -9,6 +11,20 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "sequelize",
+  cookie: {
+    maxAge: 86400,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
