239aab185c546c75916e3d04e7879988ccd2c82c
frsargua <frsargua@gmail.com>
2022-07-29 23:33:46 +0100

    added sequelize npm package

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require("path");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 const routes = require("./controllers/index");
+const sequelize = require("./config/connection");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const hbs = exphbs.create({ helpers });
 
@@ -18,6 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
