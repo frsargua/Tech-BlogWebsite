853ea729406dddc36791afc8b5176f9acb3667d6
frsargua <frsargua@gmail.com>
2022-07-29 20:42:39 +0100

    Created Basic Routes for the page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
 
-router.get("/", async (req, res) => {
+router.get("*", async (req, res) => {
   res.render("all");
+});
+
+router.get("/dashBoard", async (req, res) => {
+  res.render("dashBoard");
+});
+router.get("/SignIn", async (req, res) => {
+  res.render("SignIn");
+});
+
+router.get("/Sign Up", async (req, res) => {
+  res.render("SignUp");
 });
 
 module.exports = router;
