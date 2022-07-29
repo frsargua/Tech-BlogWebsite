4fd643dd30a599a112a1932341b67a6750fd2ef3
frsargua <frsargua@gmail.com>
2022-07-29 22:25:43 +0100

    Created User model

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        console.log(newUserData.dataValues.password);
+        newUserData.password = await bcrypt.hash(
+          newUserData.dataValues.password,
+          10
+        );
+        console.log(newUserData.dataValues);
+
+        console.log("hashed");
+        // return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
