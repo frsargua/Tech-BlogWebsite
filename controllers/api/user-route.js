e33926497dca6769cf4765904d556294d5949272
frsargua <frsargua@gmail.com>
2022-07-30 16:04:03 +0100

    redirect to homepage if logged in

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -38,7 +38,7 @@ router.post("/signIn", async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      res.json({ user: userData, message: "You are now logged in!" });
+      res.status(200).redirect("/");
     });
   } catch (err) {
     res.status(400).json(err);
