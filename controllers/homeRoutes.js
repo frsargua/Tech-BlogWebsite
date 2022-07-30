2640609a92d9a13e7a4f23ae8eb6d71b9a5a67ee
frsargua <frsargua@gmail.com>
2022-07-30 16:04:34 +0100

    get post by post_id and get all posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,17 +3,28 @@ const { User, Post } = require("../models");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  res.render("all");
+  const allPost = await Post.findAll({
+    raw: true,
+  });
+  console.log(allPost);
+  res.render("all", { allPost });
 });
 
 router.get("/dashBoard", async (req, res) => {
   res.render("dashBoard");
 });
+router.get("/post/:id", async (req, res) => {
+  const singlePost = await Post.findByPk(req.params.id, {
+    raw: true,
+  });
+  console.log(singlePost);
+  res.render("singlePost", { singlePost });
+});
 router.get("/SignIn", async (req, res) => {
   res.render("SignIn");
 });
 
-router.get("/Sign Up", async (req, res) => {
+router.get("/SignUp", async (req, res) => {
   res.render("SignUp");
 });
 
