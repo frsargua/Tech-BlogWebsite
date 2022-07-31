071103b9d34afc1b6c47eed3f55abeeea370fb23
frsargua <frsargua@gmail.com>
2022-08-01 00:52:11 +0100

    code for adding new comments and updating post

--- public/js/index.js
+++ public/js/index.js
@@ -68,3 +68,79 @@ async function signOut() {
     console.error("Error in POST request:", error);
   }
 }
+
+let commentFormEl = document.getElementById("commentForm");
+if (commentFormEl) {
+  commentFormEl.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    let newComment = new FormData(commentFormEl);
+    let newCommentProps = Object.fromEntries(newComment);
+
+    let postId = window.location.href.substring(
+      window.location.href.lastIndexOf("/") + 1
+    );
+
+    const response = await fetch(`/api/comment/${postId}`, {
+      method: "POST",
+      body: JSON.stringify(newCommentProps),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      window.location.href = "/";
+    }
+  });
+}
+
+let mainContainer = $("#posts-container");
+mainContainer.on("click", async function (e) {
+  let clickedEl = e.target;
+  if (clickedEl.hasAttribute("key")) {
+    let keyEl = clickedEl.getAttribute("key");
+    const something = document
+      .querySelectorAll(`[data-key='${keyEl}']`)
+      .forEach((el) => {
+        el.disabled = false;
+      });
+    clickedEl.nextElementSibling.classList.remove("d-none");
+    clickedEl.classList.add("d-none");
+  } else if (clickedEl.hasAttribute("key-send")) {
+    let keySendEl = clickedEl.getAttribute("key-send");
+    let formEl = document.getElementById(`updateForm${keySendEl}`);
+    // formEl.getAttribute("post-id");
+    const signUpData = new FormData(formEl);
+    let signUpFormProps = Object.fromEntries(signUpData);
+    let postId = formEl.getAttribute("post-id");
+
+    const response = await fetch(`/api/post/update/${postId}`, {
+      method: "PUT",
+      body: JSON.stringify(signUpFormProps),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    clickedEl.classList.add("d-none");
+    clickedEl.previousElementSibling.classList.remove("d-none");
+    const something = document
+      .querySelectorAll(`[data-key='${keySendEl}']`)
+      .forEach((el) => {
+        el.disabled = true;
+      });
+
+    // if (response.ok) {
+    //   window.location.href = "/";
+    // }
+  } else if (clickedEl.hasAttribute("key-delete")) {
+    let keyDeleteEl = clickedEl.getAttribute("key-delete");
+    console.log(`/api/post/${keyDeleteEl}`);
+
+    const response = await fetch(`/api/post/${keyDeleteEl}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+});
+
+function sendData(e) {
+  e.preventDefault();
+  alert("true");
+}
