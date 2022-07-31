ccda9458633733c77da3069262b82deffc643d23
frsargua <frsargua@gmail.com>
2022-07-31 13:23:17 +0100

    created relationship between comments and posts

--- models/index.js
+++ models/index.js
@@ -1,12 +1,18 @@
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
 
 User.hasMany(Post, {
   foreignKey: "post_owner",
   onDelete: "CASCADE",
 });
-
 Post.belongsTo(User, { foreignKey: "post_owner" });
+
+Post.hasMany(Comment, {
+  foreignKey: "comment_post_id",
+  onDelete: "CASCADE",
+});
+Comment.belongsTo(Post, { foreignKey: "comment_post_id" });
 
 module.exports = {
   User,
