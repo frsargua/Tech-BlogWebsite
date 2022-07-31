0825027cf9617bce0e17d0462aa0d8d52bcb8506
frsargua <frsargua@gmail.com>
2022-07-31 13:23:04 +0100

    created comment model

---
+++ models/Comment.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+const bcrypt = require("bcrypt");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+    comment_owner: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    comment_dateCreated: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+      allowNull: false,
+    },
+    comment_text: {
+      type: DataTypes.TEXT("long"),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
