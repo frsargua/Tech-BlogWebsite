db35ba269499f2faaaf64f656c2cb290a6ef24b6
frsargua <frsargua@gmail.com>
2022-08-02 16:13:02 +0100

    auth middleware

---
+++ controllers/auth/index.js
@@ -0,0 +1,56 @@
+const { User } = require("../../models/index");
+
+const login_post = async (req, res) => {
+  const { user_name, password } = req.body;
+
+  const userData = await User.findOne({
+    where: { user_name: user_name },
+  });
+  if (!userData) {
+    req.session.error = "Incorrect username";
+    return res.status(400).redirect("/signin");
+  }
+
+  const validPassword = await userData.checkPassword(password);
+
+  if (!validPassword) {
+    req.session.error = "Invalid password";
+    return res.status(400).redirect("/signin");
+  }
+
+  req.session.save(() => {
+    req.session.user_id = userData.id;
+    req.session.user_name = userData.user_name;
+    req.session.logged_in = true;
+    res.redirect("/dashboard");
+  });
+};
+
+const singUp_post = async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.logged_in = true;
+      req.session.user_id = userData.id;
+      res.status(200).redirect("/");
+    });
+  } catch (err) {
+    res.status(400).redirect("/SignUp");
+  }
+};
+
+const signOut_get = async (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.redirect("/SignIn");
+    });
+  } else {
+    res.status(404).end();
+  }
+};
+
+module.exports = {
+  login_post,
+  singUp_post,
+  signOut_get,
+};
