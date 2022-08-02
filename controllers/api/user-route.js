9ea30a0c3e8120ba9009c9d0938e11115668fa61
frsargua <frsargua@gmail.com>
2022-08-02 15:03:39 +0100

    added middleware

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -1,60 +1,13 @@
 const router = require("express").Router();
 const { User } = require("../../models");
+const logger = require("../../utils/logger");
+const { login_post, singUp_post, signOut_get } = require("../appContoller");
 
-router.post("/signUp", async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-    req.session.save(() => {
-      req.session.logged_in = true;
-      req.session.user_id = userData.id;
-      res.status(200).redirect("/");
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+router.post("/signUp", singUp_post);
 
-router.post("/signIn", async (req, res) => {
-  try {
-    const userData = await User.findOne({
-      where: { user_name: req.body.user_name },
-    });
+router.post("/signIn", login_post);
 
-    if (!userData) {
-      res.status(400).json({ message: "Incorrect username" });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email orr password, please try again" });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      res.redirect("/");
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.post("/signOut", async (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
-
-// //Optional
+router.get("/signOut", signOut_get);
 
 router.put("/", async (req, res) => {
   try {
