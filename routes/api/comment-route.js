6fad1294ae659545fc4afa94e045c40f5d62c212
frsargua <frsargua@gmail.com>
2022-08-03 11:27:44 +0100

    update comment api route

--- routes/api/comment-route.js
+++ routes/api/comment-route.js
@@ -4,6 +4,7 @@ const {
   getAllComments_get,
   createComment_post,
   deleteComment_delete,
+  updateComment_put,
 } = require("../../controllers/api/comment");
 
 router.get("/", getAllComments_get);
@@ -11,5 +12,7 @@ router.get("/", getAllComments_get);
 router.post("/:id", createComment_post);
 
 router.delete("/:id", deleteComment_delete);
+
+router.post("/update/:id/:location", updateComment_put);
 
 module.exports = router;
